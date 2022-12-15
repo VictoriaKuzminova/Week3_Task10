71cc266495bc5f4b6e670eb67649af142699e581
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-15 13:18:25 +0300

    Add files via upload

---
+++ Графическое приложение 10_1/1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.k1_TextBox = new System.Windows.Forms.RichTextBox();
+            this.all_TextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // k1_TextBox
+            // 
+            this.k1_TextBox.Location = new System.Drawing.Point(12, 134);
+            this.k1_TextBox.Name = "k1_TextBox";
+            this.k1_TextBox.Size = new System.Drawing.Size(595, 326);
+            this.k1_TextBox.TabIndex = 1;
+            this.k1_TextBox.Text = "";
+            // 
+            // all_TextBox
+            // 
+            this.all_TextBox.Location = new System.Drawing.Point(638, 134);
+            this.all_TextBox.Name = "all_TextBox";
+            this.all_TextBox.Size = new System.Drawing.Size(284, 326);
+            this.all_TextBox.TabIndex = 3;
+            this.all_TextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Информация о папке К1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Информация о папке К2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(254, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Информация о папке ALL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 483);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.all_TextBox);
+            this.Controls.Add(this.k1_TextBox);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Работа с директориями";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox k1_TextBox;
+        private System.Windows.Forms.RichTextBox all_TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Графическое приложение 10_1/1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Print(DirectoryInfo dir)
+        {
+            FileInfo[] info = dir.GetFiles();
+            k1_TextBox.Text+= "Содержимое о файлах в папках\n";
+            foreach (FileInfo file in info)
+            {
+                k1_TextBox.Text += "Имя файла: " + file.Name.ToString() + '\n' +
+                   "Расширение файла: " + file.Extension.ToString() + '\n' +
+                    "Размер файла (в байтах): " + file.Length.ToString() + '\n' +
+                    "Время создания файла: " + file.CreationTime.ToString() + '\n';
+                k1_TextBox.Text += "\n";
+            }
+        }
+        private void Print_1(DirectoryInfo dir)
+        {
+            FileInfo[] info3 = dir.GetFiles();
+
+            foreach (FileInfo file in info3)
+            {
+                all_TextBox.Text+="Имя файла: " + file.Name.ToString() + '\n' +
+                   "Расширение файла: " + file.Extension.ToString() + '\n' +
+                    "Размер файла (в байтах): " + file.Length.ToString() + '\n' +
+                    "Файл доступен только для чтения или нет (True - Только для чтения, False - для чтения и записи): " + file.IsReadOnly.ToString() + '\n' +
+                   "Время создания файла: " + file.CreationTime.ToString() + '\n' +
+                    "Последнее время записи файла: " + file.LastWriteTime.ToString()+"\n";
+                all_TextBox.Text += "\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Создание папок
+            if (Directory.Exists("C:\\temp"))
+            {
+                Directory.Delete("C:\\temp", true);
+            }
+            else
+            {
+                Directory.CreateDirectory("C:\\temp");
+            }
+            Directory.CreateDirectory("C:\\temp\\K1");
+            Directory.CreateDirectory("C:\\temp\\K2");
+            //Создание файла t1.txt
+            StreamWriter t1 = new StreamWriter("C:\\temp\\K1\\t1.txt", false);
+            t1.WriteLine("Кузьминова Наталья Михайловна, 1983 года рождения, место жительства р.п.Гремячево");
+            t1.Close();
+            //Создание файла t2.txt
+            StreamWriter t2 = new StreamWriter("C:\\temp\\K1\\t2.txt", false);
+            t2.WriteLine("Кузьминов Виктор Иванович, 1982 года рождения, место жительства р.п.Гремячево");
+            t2.Close();
+            //Создание файла t3.txt
+            StreamWriter t3 = new StreamWriter("C:\\temp\\K2\\t3.txt", false);
+            string str;
+            StreamReader sr_t1 = new StreamReader("C:\\temp\\K1\\t1.txt");
+            str = sr_t1.ReadToEnd();
+            sr_t1.Close();
+            t3.WriteLine(str);
+            StreamReader sr_t2 = new StreamReader("C:\\temp\\K1\\t2.txt");
+            str = sr_t2.ReadToEnd();
+            sr_t2.Close();
+            t3.WriteLine(str);
+            t3.Close();
+            //Информация о файлах
+            DirectoryInfo dir1 = new DirectoryInfo("C:\\temp\\K1");
+            DirectoryInfo dir2 = new DirectoryInfo("C:\\temp\\K2");
+            Print(dir1);
+            Print(dir2);
+            //Перемещение
+            File.Move("C:\\temp\\K1\\t2.txt", "C:\\temp\\K2\\t2.txt");
+            //Копирование с перезапесью(если файл есть)
+            File.Copy("C:\\temp\\K1\\t1.txt", "C:\\temp\\K2\\t1.txt", true);
+            //Переменование файла K2 в ALL
+            Directory.Move("C:\\temp\\K2", "C:\\temp\\ALL");
+            //Удаление папки K1 и все файлы в нём
+            Directory.Delete("C:\\temp\\K1", true);
+
+            Console.WriteLine("\nИнформация в папке ALL:\n");
+            DirectoryInfo dir3 = new DirectoryInfo("C:\\temp\\ALL");
+            Print_1(dir3);
+        }
+    }
+}
